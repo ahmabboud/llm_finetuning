fe0f7625107edd4ca77ca02663eae96d9317f7f7
Ahmad Abboud <ahm.abboud@gmail.com>
2024-06-29 18:26:14 -0400

    Streamlit application added

--- Deloitte/streamlit/app.py
+++ Deloitte/streamlit/app.py
@@ -1,143 +1,89 @@
-import gradio as gr
+# !pip install -qq langchain wget llama-index cohere
+# !pip install --upgrade --force-reinstall --no-cache-dir llama-cpp-python
+# %pip install llama-index-embeddings-huggingface
+# %pip install llama-index-llms-llama-cpp
+# import wget 
+
+# def bar_custom(current, total, width=80):
+#     print("Downloading %d%% [%d / %d] bytes" % (current / total * 100, current, total))
+
+
+
+# !pip -q install streamlit
+
+# %%writefile app.py
+import streamlit as st 
 import os
-import spaces
-from transformers import GemmaTokenizer, AutoModelForCausalLM
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
-from threading import Thread
+# from llama_index import (
+#   SimpleDirectoryReader,
+#   VectorStoreIndex,
+#   ServiceContext,
+# )
+from llama_index.llms.llama_cpp import LlamaCPP
+from llama_index.llms.llama_cpp.llama_utils import (
+    messages_to_prompt,
+    completion_to_prompt,
+)
+from langchain.schema import(SystemMessage, HumanMessage, AIMessage)
+os.chdir("../../../../../../")
+os.chdir("/fs01/home/ws_aabboud/finetuning-and-alignment/Deloitte/finetuned/")
+def init_page() -> None:
+  st.set_page_config(
+    page_title="Personal Chatbot"
+  )
+  st.header("Persoanl Chatbot")
+  st.sidebar.title("Options")
 
-# Set an environment variable
-HF_TOKEN = os.environ.get("HF_TOKEN", None)
+def select_llm() -> LlamaCPP:
+  return LlamaCPP(
+    model_path="models/llama-3-8b-chat-doctor.gguf",
+    temperature=0.1,
+    max_new_tokens=500,
+    context_window=3900,
+    generate_kwargs={},
+    model_kwargs={"n_gpu_layers":1},
+    messages_to_prompt=messages_to_prompt,
+    completion_to_prompt=completion_to_prompt,
+    verbose=True,
+  )
 
+def init_messages() -> None:
+  clear_button = st.sidebar.button("Clear Conversation", key="clear")
+  if clear_button or "messages" not in st.session_state:
+    st.session_state.messages = [
+      SystemMessage(
+        content="you are a helpful AI assistant. Reply your answer in markdown format."
+      )
+    ]
 
-DESCRIPTION = '''
-<div>
-<h1 style="text-align: center;">Meta Llama3 8B</h1>
-<p>This Space demonstrates the instruction-tuned model <a href="https://huggingface.co/meta-llama/Meta-Llama-3-8B-Instruct"><b>Meta Llama3 8b Chat</b></a>. Meta Llama3 is the new open LLM and comes in two sizes: 8b and 70b. Feel free to play with it, or duplicate to run privately!</p>
-<p>🔎 For more details about the Llama3 release and how to use the model with <code>transformers</code>, take a look <a href="https://huggingface.co/blog/llama3">at our blog post</a>.</p>
-<p>🦕 Looking for an even more powerful model? Check out the <a href="https://huggingface.co/chat/"><b>Hugging Chat</b></a> integration for Meta Llama 3 70b</p>
-</div>
-'''
+def get_answer(llm, messages) -> str:
+  response = llm.complete(messages)
+  return response.text
 
-LICENSE = """
-<p/>
----
-Built with Meta Llama 3
-"""
+def main() -> None:
+  init_page()
+  llm = select_llm()
+  init_messages()
 
-PLACEHOLDER = """
-<div style="padding: 30px; text-align: center; display: flex; flex-direction: column; align-items: center;">
-   <img src="https://ysharma-dummy-chat-app.hf.space/file=/tmp/gradio/8e75e61cc9bab22b7ce3dec85ab0e6db1da5d107/Meta_lockup_positive%20primary_RGB.jpg" style="width: 80%; max-width: 550px; height: auto; opacity: 0.55;  "> 
-   <h1 style="font-size: 28px; margin-bottom: 2px; opacity: 0.55;">Meta llama3</h1>
-   <p style="font-size: 18px; margin-bottom: 2px; opacity: 0.65;">Ask me anything...</p>
-</div>
-"""
-
-
-css = """
-h1 {
-  text-align: center;
-  display: block;
-}
-#duplicate-button {
-  margin: auto;
-  color: white;
-  background: #1565c0;
-  border-radius: 100vh;
-}
-"""
-
-# Load the tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", device_map="auto")  # to("cuda:0") 
-terminators = [
-    tokenizer.eos_token_id,
-    tokenizer.convert_tokens_to_ids("<|eot_id|>")
-]
-
-@spaces.GPU(duration=120)
-def chat_llama3_8b(message: str, 
-              history: list, 
-              temperature: float, 
-              max_new_tokens: int
-             ) -> str:
-    """
-    Generate a streaming response using the llama3-8b model.
-    Args:
-        message (str): The input message.
-        history (list): The conversation history used by ChatInterface.
-        temperature (float): The temperature for generating the response.
-        max_new_tokens (int): The maximum number of new tokens to generate.
-    Returns:
-        str: The generated response.
-    """
-    conversation = []
-    for user, assistant in history:
-        conversation.extend([{"role": "user", "content": user}, {"role": "assistant", "content": assistant}])
-    conversation.append({"role": "user", "content": message})
-
-    input_ids = tokenizer.apply_chat_template(conversation, return_tensors="pt").to(model.device)
+  if user_input := st.chat_input("Input your question!"):
+    st.session_state.messages.append(HumanMessage(content=user_input))
+    with st.spinner("Bot is typing ..."):
+      answer = get_answer(llm, user_input)
+      print(answer)
+    st.session_state.messages.append(AIMessage(content=answer))
     
-    streamer = TextIteratorStreamer(tokenizer, timeout=10.0, skip_prompt=True, skip_special_tokens=True)
 
-    generate_kwargs = dict(
-        input_ids= input_ids,
-        streamer=streamer,
-        max_new_tokens=max_new_tokens,
-        do_sample=True,
-        temperature=temperature,
-        eos_token_id=terminators,
-    )
-    # This will enforce greedy generation (do_sample=False) when the temperature is passed 0, avoiding the crash.             
-    if temperature == 0:
-        generate_kwargs['do_sample'] = False
-        
-    t = Thread(target=model.generate, kwargs=generate_kwargs)
-    t.start()
+  messages = st.session_state.get("messages", [])
+  for message in messages:
+    if isinstance(message, AIMessage):
+      with st.chat_message("assistant"):
+        st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+      with st.chat_message("user"):
+        st.markdown(message.content)
 
-    outputs = []
-    for text in streamer:
-        outputs.append(text)
-        #print(outputs)
-        yield "".join(outputs)
-        
-
-# Gradio block
-chatbot=gr.Chatbot(height=450, placeholder=PLACEHOLDER, label='Gradio ChatInterface')
-
-with gr.Blocks(fill_height=True, css=css) as demo:
-    
-    gr.Markdown(DESCRIPTION)
-    gr.DuplicateButton(value="Duplicate Space for private use", elem_id="duplicate-button")
-    gr.ChatInterface(
-        fn=chat_llama3_8b,
-        chatbot=chatbot,
-        fill_height=True,
-        additional_inputs_accordion=gr.Accordion(label="⚙️ Parameters", open=False, render=False),
-        additional_inputs=[
-            gr.Slider(minimum=0,
-                      maximum=1, 
-                      step=0.1,
-                      value=0.95, 
-                      label="Temperature", 
-                      render=False),
-            gr.Slider(minimum=128, 
-                      maximum=4096,
-                      step=1,
-                      value=512, 
-                      label="Max new tokens", 
-                      render=False ),
-            ],
-        examples=[
-            ['How to setup a human base on Mars? Give short answer.'],
-            ['Explain theory of relativity to me like I’m 8 years old.'],
-            ['What is 9,000 * 9,000?'],
-            ['Write a pun-filled happy birthday message to my friend Alex.'],
-            ['Justify why a penguin might make a good king of the jungle.']
-            ],
-        cache_examples=False,
-                     )
-    
-    gr.Markdown(LICENSE)
-    
 if __name__ == "__main__":
-    demo.launch()
+
+  main()
+ 
+# !streamlit run app.py & npx localtunnel --port 8501
